e6f6b4fbdd2db9e5332f5927e23c94c6c0fd0d25
unknown <krishagarwal.ka21@gmail.com>
2023-07-19 22:42:16 +0530

    testing data

--- app.py
+++ app.py
@@ -18,20 +18,15 @@ def index():
 
 @app.route('/upload', methods=['POST'])
 def upload():
-    # os.chmod(r'./Tesseract-OCR/tesseract.exe', 0o0777)
-    
-    pytesseract.pytesseract.tesseract_cmd = r'/usr/bin/tesseract'
+
+    pytesseract.pytesseract.tesseract_cmd = r'./Tesseract-OCR/tesseract.exe'
 
 
     # Get the uploaded image file from the form
     image_file = request.files['image']
 
-    print(image_file)
-
     # Open the image using PIL
-    # image = Image.open(image_file)
-    # image.tobytes("xbm", "rgb")
-    image = image_file.read() # open(image_file, 'rb')
+    image = Image.open(image_file)
 
     # Get the extraction time from the form
     extraction_time_str = request.form['extraction_time']
@@ -40,23 +35,57 @@ def upload():
     extraction_time = datetime.strptime(extraction_time_str, '%Y-%m-%dT%H:%M')
 
     # Calculate the time difference from the current time to the extraction time
-    now_time = datetime.utcnow() + timedelta(hours=5.5) #datetime.datetime.now()
+    now_time = datetime.utcnow() + timedelta(hours=5.5)
     if extraction_time > now_time:
         dif = (extraction_time - now_time).total_seconds()
     else:
         dif = 0
-    # time.sleep(dif)
-    files = {'image': image}
-    r = requests.post(API_URL,headers={'X-Api-Key': 'ycee22h9ItBexlq6lKbvJQ==Kaa1FJCgG7VllCBc'}, files=files)
-    json_rst = r.json()
-    extracted_text = " ".join([i['text'] for i in json_rst])
+    extracted_text = pytesseract.image_to_string(image)
 
-    # extracted_text = pytesseract.image_to_string(image)
     return render_template('result.html', text=extracted_text, timer = dif)
 
-
-
     return render_template('result.html')
+
+
+    # os.chmod(r'./Tesseract-OCR/tesseract.exe', 0o0777)
+    
+    # pytesseract.pytesseract.tesseract_cmd = r'/usr/bin/tesseract'
+
+
+    # # Get the uploaded image file from the form
+    # image_file = request.files['image']
+
+    # print(image_file)
+
+    # # Open the image using PIL
+    # # image = Image.open(image_file)
+    # # image.tobytes("xbm", "rgb")
+    # image = image_file.read() # open(image_file, 'rb')
+
+    # # Get the extraction time from the form
+    # extraction_time_str = request.form['extraction_time']
+
+    # # Parse the extraction time as a datetime object
+    # extraction_time = datetime.strptime(extraction_time_str, '%Y-%m-%dT%H:%M')
+
+    # # Calculate the time difference from the current time to the extraction time
+    # now_time = datetime.utcnow() + timedelta(hours=5.5) #datetime.datetime.now()
+    # if extraction_time > now_time:
+    #     dif = (extraction_time - now_time).total_seconds()
+    # else:
+    #     dif = 0
+    # # time.sleep(dif)
+    # files = {'image': image}
+    # r = requests.post(API_URL,headers={'X-Api-Key': 'ycee22h9ItBexlq6lKbvJQ==Kaa1FJCgG7VllCBc'}, files=files)
+    # json_rst = r.json()
+    # extracted_text = " ".join([i['text'] for i in json_rst])
+
+    # # extracted_text = pytesseract.image_to_string(image)
+    # return render_template('result.html', text=extracted_text, timer = dif)
+
+
+
+    # return render_template('result.html')
 
 # def perform_extraction(image):
 #     # Perform OCR on the image
